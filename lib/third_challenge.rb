52d622dd84d365702e71ba4dec7994ce8b66e68e
Eric Fiorani <efiorani305@gmail.com>
2018-05-01 17:00:38 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -27,8 +27,8 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        :name => "Lord Montague",
-        :age =>  "53"
+        :name => "Lord Capulet",
+        :age =>  "51"
         },
       :heroine => {
         #your key/value pairs here
